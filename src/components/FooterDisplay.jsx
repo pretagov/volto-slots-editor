6951f3a689fd0bba39380a21357dacbc2cff3765
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-09-19 00:05:00 +0100

    Remove unused imports

--- src/components/FooterDisplay.jsx
+++ src/components/FooterDisplay.jsx
@@ -1,10 +1,6 @@
 import { useVoltoBlocksFooter } from '@plone-collective/volto-blocks-footer';
 import { RenderBlocks } from '@plone/volto/components';
-import {
-  blockNeedsSection,
-  blocksNeedSections,
-  sectionFields,
-} from 'nsw-design-system-plone6/components/Blocks/Section/utils';
+import { blocksNeedSections } from 'nsw-design-system-plone6/components/Blocks/Section/utils';
 import { BlocksAsSection } from 'nsw-design-system-plone6/components/Components/Section';
 
 export function FooterDisplay(props) {
