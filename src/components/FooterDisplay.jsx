202e380afeba931fa24802b9784200d6406c7536
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-09-19 02:31:25 +0100

    Remove direct Section support

--- src/components/FooterDisplay.jsx
+++ src/components/FooterDisplay.jsx
@@ -1,23 +1,11 @@
 import { useVoltoBlocksFooter } from '@plone-collective/volto-blocks-footer';
 import { RenderBlocks } from '@plone/volto/components';
-import { blocksNeedSections } from 'nsw-design-system-plone6/components/Blocks/Section/utils';
-import { BlocksAsSection } from 'nsw-design-system-plone6/components/Components/Section';
 
 export function FooterDisplay(props) {
   const footerBlockData = useVoltoBlocksFooter();
 
   if (!footerBlockData) {
     return null;
-  }
-
-  if (blocksNeedSections(footerBlockData?.blocks)) {
-    return (
-      <BlocksAsSection
-        blocksLayout={footerBlockData['blocks_layout'].items}
-        blocksData={footerBlockData.blocks}
-        // content={content}
-      />
-    );
   }
 
   return <RenderBlocks {...props} content={footerBlockData} />;
