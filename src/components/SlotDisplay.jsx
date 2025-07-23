95537d5b43f575697514225b0137aab2a3c999d7
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2025-07-23 11:39:44 +0100

    Fix blocks not getting the correct `path` passed to them

--- src/components/SlotDisplay.jsx
+++ src/components/SlotDisplay.jsx
@@ -1,8 +1,13 @@
 import { useVoltoSlotsEditor } from '@plone-collective/volto-slots-editor';
 import { RenderBlocks } from '@plone/volto/components';
+import { getBaseUrl } from '@plone/volto/helpers';
+import { useLocation } from 'react-router';
 
 export function SlotDisplay({ slot, ...props }) {
   const blockData = useVoltoSlotsEditor(slot);
+  const { pathname } = useLocation();
+  // Volto would normally pass path as a prop, but that means you have to always pass it at the component callsite which is messy.
+  const path = props.path || getBaseUrl(pathname || '');
 
   if (!blockData) {
     return null;
@@ -12,5 +17,5 @@ export function SlotDisplay({ slot, ...props }) {
     return null;
   }
 
-  return <RenderBlocks {...props} content={blockData} />;
+  return <RenderBlocks {...props} content={blockData} path={path} />;
 }
